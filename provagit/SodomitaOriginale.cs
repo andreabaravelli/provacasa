ab4f1dd25fe3b0a92346f2775cdbbc49594400e3
ORIANO <andrea.baravelli.stud@ispascalcomandini.it>
2022-11-16 17:22:40 +0100

    sesso anale

---
+++ provagit/SodomitaOriginale.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provagit
+{
+    internal class SodomitaOriginale
+    {
+        internal string Nome { get;  set; } = "tonetti";
+
+        internal double length { get;  set; }
+        internal SodomitaOriginale()
+        {
+
+
+        }
+
+        internal void lunghezzaMinima()
+        {
+            if (Nome=="Tonetti")
+            {
+
+                length = 0.0000000000000000000000000000000000001;
+
+            }
+
+
+        }
+
+    }
+}
