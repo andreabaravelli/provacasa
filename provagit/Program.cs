c71f1d8bb1afb15cfebb5994f61c90ada2778dd6
ORIANO <andrea.baravelli.stud@ispascalcomandini.it>
2022-11-16 16:52:34 +0100

    versione tonetti base

---
+++ provagit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provagit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("homosessual"); // sodomita 
+
+            Console.Read();
+        }
+    }
+}
